0a733ac428c7afbba2c00027907e50e228aedcd5
AlbinOlausson <albin.olausson@gmail.com>
2014-11-18 13:02:21 +0100

    first commit

---
+++ src/network/Server.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+  public static void main(String[] args) throws Exception {
+    ServerSocket serverSocket = new ServerSocket(12345);
+    int id = 0;
+    while (true) {
+      Socket clientSocket = serverSocket.accept();
+      ClientThread clientThread = new ClientThread(clientSocket, id++);
+      clientThread.start();
+    }
+  }
+}
+
+class ClientThread extends Thread {
+  Socket clientSocket;
+  int clientID = -1;
+  boolean running = true;
+
+  ClientThread(Socket s, int i) {
+    clientSocket = s;
+    clientID = i;
+  }
+
+  public void run() {
+    System.out.println("Accepted Client : ID - " + clientID + " : Address - "
+        + clientSocket.getInetAddress().getHostName());
+    try {
+      BufferedReader   in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      PrintWriter   out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+      
+      while (running) {
+        String clientCommand = in.readLine();
+        System.out.println("Client Says :" + clientCommand);
+        if (clientCommand.equalsIgnoreCase("quit")) {
+          running = false;
+          System.out.print("Stopping client thread for client : " + clientID);
+        } else {
+          out.println(clientCommand);
+          out.flush();
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
