cd902ca143428564e795f8c44063712aefe16ac5
Axellent <axel.sigl@hotmail.com>
2014-11-18 19:09:21 +0100

    IO thread and server now looks for an available port

--- src/network/Server.java
+++ src/network/Server.java
@@ -1,52 +1,113 @@
+package network;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class Server {
-  public static void main(String[] args) throws Exception {
-    ServerSocket serverSocket = new ServerSocket(12345);
-    int id = 0;
-    while (true) {
-      Socket clientSocket = serverSocket.accept();
-      ClientThread clientThread = new ClientThread(clientSocket, id++);
-      clientThread.start();
-    }
-  }
+/**
+ * 
+ * @author Axel Sigl
+ *
+ */
+public class Server{
+
+	/**
+	 * 
+	 * @author Axel Sigl
+	 */
+	public Server() {
+		try {
+			runServer();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @author Axel Sigl
+	 * @throws Exception
+	 */
+	private void runServer() throws Exception {
+		ServerSocket serverSocket = new ServerSocket(0);
+		System.out.println("Server started");
+		
+		new IOThread().start();
+		
+		int id = 0;
+		while(true) {
+			Socket clientSocket = serverSocket.accept();
+			ClientThread clientThread = new ClientThread(clientSocket, id++);
+			clientThread.start();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Server();
+	}
+}
+
+/**
+ * 
+ * @author Axel Sigl
+ *
+ */
+class IOThread extends Thread{
+	Scanner in = new Scanner(System.in);
+	String input = "";
+	
+	public void run(){
+		System.out.println("Started IO thread");
+		
+		while(true){
+			input = in.nextLine();
+			System.out.println("Parsing command: " + input);
+			
+			if(input.equals("q")){
+				System.out.println("Server shutting down");
+				System.exit(0);
+			}
+		}
+	}
 }
 
 class ClientThread extends Thread {
-  Socket clientSocket;
-  int clientID = -1;
-  boolean running = true;
+	Socket clientSocket;
+	int clientID = -1;
+	boolean running = true;
 
-  ClientThread(Socket s, int i) {
-    clientSocket = s;
-    clientID = i;
-  }
+	ClientThread(Socket s, int i) {
+		clientSocket = s;
+		clientID = i;
+	}
 
-  public void run() {
-    System.out.println("Accepted Client : ID - " + clientID + " : Address - "
-        + clientSocket.getInetAddress().getHostName());
-    try {
-      BufferedReader   in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      PrintWriter   out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-      
-      while (running) {
-        String clientCommand = in.readLine();
-        System.out.println("Client Says :" + clientCommand);
-        if (clientCommand.equalsIgnoreCase("quit")) {
-          running = false;
-          System.out.print("Stopping client thread for client : " + clientID);
-        } else {
-          out.println(clientCommand);
-          out.flush();
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	public void run() {
+		System.out
+				.println("Accepted Client : ID - " + clientID + " : Address - "
+						+ clientSocket.getInetAddress().getHostName());
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+			PrintWriter out = new PrintWriter(new OutputStreamWriter(
+					clientSocket.getOutputStream()));
+
+			while (running) {
+				String clientCommand = in.readLine();
+				System.out.println("Client Says :" + clientCommand);
+				if (clientCommand.equalsIgnoreCase("quit")) {
+					System.out.print("Stopping client thread for client : "
+							+ clientID);
+				} else {
+					out.println(clientCommand);
+					out.flush();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
